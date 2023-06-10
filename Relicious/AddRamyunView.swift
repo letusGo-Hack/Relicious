af28ec121f8cec8344bf2842ce0e06bc95822c33
Whales <Whales.w.Jin@gmail.com>
2023-06-10 16:57:36 +0900

    AddRamyunView에 취소, 저장, 라면이미지추가 버튼 생성

--- Relicious/AddRamyunView.swift
+++ Relicious/AddRamyunView.swift
@@ -12,17 +12,12 @@ struct MultiPicker: View  {
     typealias Label = String
     typealias Entry = String
     
-    
-    @State var name: String = ""
-    @State var calorie: String = ""
-    
-
     let data: [ (Label, [Entry]) ]
     @Binding var selection: [Entry]
 
     var body: some View {
         GeometryReader { geometry in
-            HStack {
+            HStack(spacing: 0) {
                 ForEach(0..<self.data.count) { column in
                     Picker(self.data[column].0, selection: self.$selection[column]) {
                         ForEach(0..<self.data[column].1.count) { row in
@@ -31,13 +26,60 @@ struct MultiPicker: View  {
                         }
                     }
                     .pickerStyle(WheelPickerStyle())
-                    .frame(width: geometry.size.width / CGFloat(self.data.count), height: geometry.size.height)
+                    .frame(width: geometry.size.width / CGFloat(self.data.count))
                     .clipped()
                 }
             }
         }
+        .padding([.leading, .trailing], 30)
+        .border(.orange)
         
-        VStack {
+    }
+    
+}
+
+struct AddRamyunView: View {
+    @State var name: String = ""
+    @State var calorie: String = ""
+    
+    @State var data: [(String, [String])] = [
+        ("One", Array(0...59).map { "\($0) 분" }),
+        ("Two", Array(0...59).map { "\($0) 초" })
+    ]
+    @State var selection: [String] = [0, 0].map { "\($0)" }
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 0) {
+            HStack {
+                Button("취소\n버튼") {
+                    // 실행할 코드
+                }
+                
+                Button("저장\n버튼") {
+                    // 실행할 코드
+                }.padding([.leading], 160)
+            }
+            .border(.red)
+            Spacer()
+            
+            MultiPicker(data: data, selection: $selection)
+                .frame(height: 200)
+            
+            Spacer()
+            HStack {
+                Image("")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .aspectRatio(contentMode: .fill)
+                    .border(.pink)
+                
+                Button("라면이미지 \n 추가버튼") {
+                    // 실행할 코드
+                }.padding([.leading, .trailing], 40)
+            }
+            .border(.purple)
+            
+            Spacer()
             HStack(alignment: .center) {
                 Text("라면명 :")
                     .font(.callout)
@@ -57,22 +99,7 @@ struct MultiPicker: View  {
                     .padding()
                     .background(Color(uiColor: .secondarySystemBackground))
             }
-        }
-    }
-    
-}
-
-struct AddRamyunView: View {
-    @State var data: [(String, [String])] = [
-        ("One", Array(0...59).map { "\($0) 분" }),
-        ("Two", Array(0...59).map { "\($0) 초" })
-    ]
-    @State var selection: [String] = [0, 0].map { "\($0)" }
-    
-    var body: some View {
-        VStack(alignment: .center) {
-//            Text(verbatim: "Selection: \(selection)")
-            MultiPicker(data: data, selection: $selection).frame(height: 300)
+            Spacer()
         }
     }
 }
