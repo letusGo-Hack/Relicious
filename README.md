62bbc3c29bdf3d2727eeb61ef874e5c55c98ac3b
dp221125 <dp221125@naver.com>
2023-06-10 12:14:48 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Relicious
+'R'amyun + Delicious
+
+## 팀원
+- 밀쿄 ☕️
+- 제이 🦄
+- 커리 🍛
+- 웨일 🐋
+## 프로젝트 소개
+- [Bring widgets to life](https://developer.apple.com/videos/play/wwdc2023/10028/)
+- 사용자가 자주 먹는 라면의 타이머를 위젯에서도 편하게 실행시킬 수 있는 앱
---
+++ Relicious/ReliciousApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReliciousApp.swift
+//  Relicious
+//
+//  Created by Milkyo on 6/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ReliciousApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
